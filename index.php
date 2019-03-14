59a07497f657d46e6521a4cb2ca0f8cf739df753
momomimo <47049931+momomimo@users.noreply.github.com>
2019-03-14 18:56:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+    <?php
+    $text = "hello word"; 
+    $nama = "Sutrimo";
+    $nim = "16.01.53.0183";
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Service</title>
+</head>
+<body>
+    
+<h1><?php echo $text;?></h1>
+<p>
+Nama    : <?php echo $nama;?><br>
+NIM     : <?php echo $nim;?>
+</p>
+    
+
+</body>
+</html>
